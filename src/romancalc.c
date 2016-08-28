8f228f39542a804a5d10b602bbe6174ada01ecff
jgartee <jlgartee@gmail.com>
2016-08-28 17:04:41 -0400

    added test and code to validate presence of the operator

--- src/romancalc.c
+++ src/romancalc.c
@@ -5,10 +5,13 @@
 int RomanCalculator(char *first, char* operator, char* second, char* result) {
 
 	if( first == NULL )
-		return ROMAN_CALCULATOR_INVALID_FIRST_TERM;
+		return ROMAN_CALCULATOR_MISSING_FIRST_TERM;
 
 	if( second == NULL )
-		return ROMAN_CALCULATOR_INVALID_SECOND_TERM;
+		return ROMAN_CALCULATOR_MISSING_SECOND_TERM;
+
+	if( operator == NULL )
+		return ROMAN_CALCULATOR_MISSING_OPERATOR;
 
 	return ROMAN_CALCULATOR_SUCCESS;
 }
