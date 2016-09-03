0afb6b4cb6b7043d88c4c3eb0e7a0ccdba452b80
jgartee <jlgartee@gmail.com>
2016-09-03 07:59:04 -0400

    Add setup/teardown fixtures and refactor

--- test/romancalctest.c
+++ test/romancalctest.c
@@ -4,24 +4,27 @@
 #include "romancalctest.h"
 #include "../src/romancalc.h"
 
+char result[16];
+
+void setup(void) {
+	memset(result, 0x00, sizeof(result));
+}
+
+void teardown(void) {
+
+}
 
 START_TEST(test_NULL_plus_I_Returns_ROMAN_CALCULATOR_MISSING_FIRST_TERM) {
-	char result[16];
-	memset(result,'\0',sizeof(result));
 	ck_assert_int_eq(RomanCalculator(NULL, "+", "I", result), ROMAN_CALCULATOR_MISSING_FIRST_TERM);
 }
 END_TEST
 
 START_TEST(test_I_plus_NULL_Returns_ROMAN_CALCULATOR_MISSING_SECOND_TERM) {
-	char result[16];
-	memset(result,'\0',sizeof(result));
 	ck_assert_int_eq(RomanCalculator("I", "+", NULL, result), ROMAN_CALCULATOR_MISSING_SECOND_TERM);
 }
 END_TEST
 
 START_TEST(test_I_NULL_I_Returns_ROMAN_CALCULATOR_MISSING_OPERATOR ) {
-	char result[16];
-	memset(result,'\0',sizeof(result));
 	ck_assert_int_eq(RomanCalculator("I", NULL, "I", result), ROMAN_CALCULATOR_MISSING_OPERATOR);
 }
 END_TEST
@@ -36,6 +39,7 @@ Suite* CalculatorSuite(void) {
 	Suite* suite = suite_create("Roman Numeral Calculator Tests");
 	TCase* inputs_case = tcase_create("Validate Arguments");
 
+	tcase_add_checked_fixture(inputs_case, setup, teardown);
 	tcase_add_test(inputs_case, test_NULL_plus_I_Returns_ROMAN_CALCULATOR_MISSING_FIRST_TERM);
 	tcase_add_test(inputs_case, test_I_plus_NULL_Returns_ROMAN_CALCULATOR_MISSING_SECOND_TERM);
 	tcase_add_test(inputs_case, test_I_NULL_I_Returns_ROMAN_CALCULATOR_MISSING_OPERATOR);
