1c2d2cae313c4c5283e960324b8253f95a215f16
jgartee <jlgartee@gmail.com>
2016-09-03 11:26:26 -0400

    Refactoring production code to remove literals

--- src/romancalc.c
+++ src/romancalc.c
@@ -23,38 +23,63 @@ int RomanCalculator(char *first, char* operator, char* second, char* result) {
 		return ROMAN_CALCULATOR_INVALID_OPERATOR;
 	}
 
-	int firstInArabic;
-	int secondInArabic;
+	int first_in_arabic;
+	int second_in_arabic;
 
-	firstInArabic = convertRomanToArabic(first);
-	secondInArabic = convertRomanToArabic(second);
-	strcpy(result, convertArabicToRoman(firstInArabic + secondInArabic));
+	first_in_arabic = convertRomanToArabic(first);
+	second_in_arabic = convertRomanToArabic(second);
+	strcpy(result, convertArabicToRoman(first_in_arabic + second_in_arabic));
 
-	printf("\nfirst = %s, second=%s, result=%s, firstInArabic=%i, secondInArabic=%i", first, second, result, firstInArabic, secondInArabic);
 	return ROMAN_CALCULATOR_SUCCESS;
 }
 
+typedef struct arabicRomanPairs {
+	int 	arabic;
+	char* 	roman;
+} arabicRomanPairs; 
+
+arabicRomanPairs arabic_roman_lookup[] = 	{
+												{   1, 	"I"  },
+												{   2, 	"II" },
+												{   3, 	"III"},
+												{   4, 	"IV" },
+												{   5, 	"V"  },
+												{   9, 	"IX" },
+												{  10, 	"X"  },
+												{  40, 	"XL" },
+												{  50, 	"L"  },
+												{  90, 	"XC" },
+												{ 100, 	"C"  },
+												{ 400, 	"CD" },
+												{ 500, 	"D"  },
+												{ 900, 	"CM" },
+												{1000, 	"M"  },
+												{  -1, 	NULL }
+											};
+
 int convertRomanToArabic(char* romanNumeral){
 
-	printf("\n\tromanNumeral=%s", romanNumeral);
+	arabicRomanPairs* pairs = arabic_roman_lookup;
 
-	if( strcmp(romanNumeral, "I") == 0 )
-		return 1;
+	while( pairs->roman != NULL ) {
+		if(strcmp(romanNumeral, pairs->roman) == 0)
+			break;
+		pairs++;
+	}
 
-	if( strcmp(romanNumeral, "II") == 0 )
-		return 2;
-
-	return 3;
+	return pairs->arabic;
 }
 
 char* convertArabicToRoman(int arabicNumber){
-	printf("\n\tarabicNumber=%i", arabicNumber);
 
-	if(arabicNumber == 1)
-		return "I";
+	arabicRomanPairs* pairs = arabic_roman_lookup;
 
-	if(arabicNumber == 2)
-		return "II";
+	while( pairs->arabic != -1 ){
+		if(pairs->arabic == arabicNumber)
+			break;
 
-	return "III";
+		pairs++;
+	}
+
+	return pairs->roman; 
 }
